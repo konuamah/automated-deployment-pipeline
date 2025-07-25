191e0329e45373d576a175ff8b7498e0168545c7
konuamah <kelvinnuamah123@gmail.com>
2025-07-25 15:59:15 +0000

    Add frontend as a regular folder

---
+++ backend/routes/books.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const Book = require('../models/Book');
+
+// Get all books
+router.get('/', async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.json(books);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Create a book
+router.post('/', async (req, res) => {
+  const book = new Book({
+    title: req.body.title,
+    author: req.body.author,
+  });
+
+  try {
+    const newBook = await book.save();
+    res.status(201).json(newBook);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+module.exports = router;
